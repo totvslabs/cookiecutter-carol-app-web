4efd2818c9f6b554e85a4a196db4b42681eb31f2
Giovani Zamboni <gzamboni@gmail.com>
2019-07-29 11:48:16 -0300

    feat: Google Analytics for cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+import http.client
 import os
+import platform
+import urllib.parse
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -7,7 +10,30 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def post_ga_data():
+    params = urllib.parse.urlencode({
+        'v': 1,
+        'tid': 'UA-144583662-1',
+        'uid': '{{ cookiecutter.email }}',
+        't' : 'event',
+        'ec': 'carol_cookiecuter',
+        'ea': 'new_web_project',
+        'el': 'carol_web_app',
+        'an': 'cookiecutter-carol-app-web',
+        'cd1': 'tenant',
+        'cm1': '{{ cookiecutter.carol_app_tenant }}',
+        'cd2': 'project',
+        'cm2': '{{ cookiecutter.project_name }}',
+        'cd3': 'os',
+        'cm3': platform.system(),
+        'cd4': 'python_version',
+        'cm4': platform.python_version()
+    })
+
+    connection = http.client.HTTPConnection('www.google-analytics.com')
+    connection.request('POST', '/collect', params)
+
+post_ga_data()
 
 if __name__ == '__main__':
     pass
-
