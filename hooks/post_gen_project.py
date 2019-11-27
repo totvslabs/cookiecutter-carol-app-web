921dc51eb29274f59f2a567ee1a2f697f92da5ed
Giovani Zamboni <gzamboni@gmail.com>
2019-11-27 10:59:51 -0300

    fix: google analytics without try catch

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -29,8 +29,11 @@ def post_ga_data():
         'cm4': platform.python_version()
     })
 
-    connection = http.client.HTTPConnection('www.google-analytics.com')
-    connection.request('POST', '/collect', params)
+    try:
+        connection = http.client.HTTPConnection('www.google-analytics.com')
+        connection.request('POST', '/collect', params)
+    except Exception as e:
+        print(e)
 
 post_ga_data()
 
